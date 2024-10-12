0eda75c028b19c699f5e78de749465f20df37f6e
Hiago Gimenez <163415252+gimenezhiago@users.noreply.github.com>
2024-10-12 14:50:59 -0300

    dsfgf

--- Login/Login.js
+++ Login/Login.js
@@ -1,13 +1,11 @@
-let btn = document.getElementById('btn')
-btn.addEventListener('click', entrar) 
-    
-    
-    function entrar(event) {
+let btn = document.getElementById('btn');
+btn.addEventListener('click', entrar);
+
+function entrar(event) {
     event.preventDefault(); 
 
     let email = document.getElementById('email').value.trim();
     let usuario = document.getElementById('usuario').value.trim();
-
   
     let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
@@ -21,4 +19,19 @@ btn.addEventListener('click', entrar)
         return;
     }
 
-};
+    let usuarioPattern = /^[a-zA-Z0-9]+$/;
+    if (!usuarioPattern.test(usuario)) {
+        alert('O nome de usuário deve conter apenas letras e números, sem espaços.');
+        return;
+    }
+
+    if (email.length > 70) {
+        alert('O email deve ter no máximo 50 caracteres.');
+        return;
+    }
+    if (usuario.length > 20) {
+        alert('O nome de usuário deve ter no máximo 20 caracteres.');
+        return;
+    }
+
+}
